741981430e3e2fe224a6993e0b7f8cd4f9110d3d
ojasvidevtamang <lalamdorje@gmail.com>
2024-10-03 08:12:26 +0545

    refractor[ControlSystemController]removes unnecessary code.

--- app/Http/Controllers/ControlSystemController.php
+++ app/Http/Controllers/ControlSystemController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ControlSystemController extends Controller
 {
-    public function index(){x``
+    public function index(){
         return view('admin.camera-control.index');
     }
 }
