44112ae549617a244091db0dd19abeaf989fc6e3
ojasvidevtamang <lalamdorje@gmail.com>
2024-10-16 09:01:12 +0545

    refractor[web.php]:adds route to add clicked photo.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControlSystemController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PhotoController;
 
 Route::get('/', function () {
     return view('admin.index');
@@ -10,3 +11,4 @@ Route::get('/', function () {
 
 Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
 Route::get('/control-system', [ControlSystemController::class, 'index'])->name('admin.control_system');
+Route::post('/save-photo', [PhotoController::class, 'store']);
