4a3930afd577240bcfefe59c56eb2baab319efa5
ojasvidevtamang <lalamdorje@gmail.com>
2024-10-16 09:03:27 +0545

    refractor[js.blade.php]:adds javascript to take image to store.

--- resources/views/admin/camera-control/js.blade.php
+++ resources/views/admin/camera-control/js.blade.php
@@ -8,6 +8,8 @@
         const cameraIcon = document.getElementById('camera-icon');
         const cameraSwitch = document.getElementById('customSwitch1')
         let sliderContainer = document.getElementById('slider-container');
+        let canvas = document.getElementById('canvas');
+        let takePhotoButton = document.getElementById('take-photo-btn');
 
         // let zoomLevel = 1;
 
@@ -35,12 +37,14 @@
         cameraSwitch.addEventListener('change', () => {
             if(cameraSwitch.checked) {
                 mirrorButton.style.display = 'block';
+                takePhotoButton.style.display = 'block';
                 sliderContainer.style.display = 'block';
                 cameraIcon.style.display = "none";
                 turnOnCamera();
             } else{
                 cameraIcon.style.display = "block";
                 mirrorButton.style.display = 'none';
+                takePhotoButton.style.display = 'none';
                 sliderContainer.style.display = 'none';
                 turnOffCamera();
             }
@@ -59,6 +63,39 @@
         function updateTransform(zoomLevel){
             let mirrorScale = isMirrored ? '-1' : '1';
             video.style.transform = `scale(${mirrorScale}, 1) scale(${zoomLevel})`;
+        }
+
+        takePhotoButton.addEventListener('click', function (){
+           const context = canvas.getContext('2d');
+           canvas.width = video.videoWidth;
+           canvas.height = video.videoHeight;
+           context.drawImage(video, 0, 0, canvas.width, canvas.height);
+           const imageData = canvas.toDataURL('image/png');
+           savePhoto(imageData);
+        });
+
+        function savePhoto(imageData){
+            fetch('/save-photo', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                },
+                body: JSON.stringify({ image: imageData })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if(data.success){
+                    alert(data.file);
+                    alert("Photo Saved Successfully");
+                } else{
+                    alert('Error Saving Photo');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
         }
 
     </script>
