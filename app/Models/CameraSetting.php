ca3f8680c8ea84e85959dc5ff92665b2d4f366fc
ojasvi264 <lalamdorje@gmail.com>
2024-10-21 18:44:22 +0900

    refractor[CameraSetting]:updates column.

--- app/Models/CameraSetting.php
+++ app/Models/CameraSetting.php
@@ -9,5 +9,5 @@ class CameraSetting extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['camera_id', 'zoom_level', 'pan_limit_max', 'pan_limit_min', 'tilt_limit_max', 'tilt_limit_max'];
+    protected $fillable = ['camera_id', 'zoom_level', 'pan_limit_max', 'pan_limit_min', 'tilt_limit_min', 'tilt_limit_max'];
 }
