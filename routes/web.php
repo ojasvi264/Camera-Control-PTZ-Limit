0525aab432464ed2abc4cacb1fb1d69dc2873ca6
ojasvidevtamang <lalamdorje@gmail.com>
2024-10-21 11:05:22 +0545

    refractor[web.php]:adds route for camera setting and ptz.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControlSystemController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\CameraSettingController;
 
 Route::get('/', function () {
     return view('admin.index');
@@ -11,4 +12,7 @@ Route::get('/', function () {
 
 Route::get('/', [DashboardController::class, 'index'])->name('admin.index');
 Route::get('/control-system', [ControlSystemController::class, 'index'])->name('admin.control_system');
+Route::get('/camera-setting', [CameraSettingController::class, 'index'])->name('admin.ptz_setting');
+Route::post('ptz/store', [CameraSettingController::class, 'store'])->name('admin.store_ptz');
 Route::post('/save-photo', [PhotoController::class, 'store']);
+Route::get('camera/info', [CameraSettingController::class, 'getCameraInfo']);
