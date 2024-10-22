3c7dc031553fce1df070633a5c7b62c10c569132
ojasvi264 <lalamdorje@gmail.com>
2024-10-22 17:30:55 +0900

    refractor[ptz_info]:adds ptz listings.

--- resources/views/admin/camera-setting/ptz_info.blade.php
+++ resources/views/admin/camera-setting/ptz_info.blade.php
@@ -1,4 +1,26 @@
 @extends('admin.template.layout.app')
 @section('content')
-
+    <div class="content">
+        <table class="table">
+            <tr>
+                <th>S.N</th>
+                <th>Zoom Level</th>
+                <th>Min PAN</th>
+                <th>Max PAN</th>
+                <th>Min Tilt</th>
+                <th>Max Tilt</th>
+            </tr>
+            @foreach($ptzSettings as $index => $ptzSetting)
+                <tr>
+                    <td>{{++$index}}</td>
+                    <td>{{$ptzSetting->zoom_level}}</td>
+                    <td>{{$ptzSetting->pan_limit_min}}</td>
+                    <td>{{$ptzSetting->pan_limit_max}}</td>
+                    <td>{{$ptzSetting->tilt_limit_min}}</td>
+                    <td>{{$ptzSetting->tilt_limit_max}}</td>
+                </tr>
+            @endforeach
+        </table>
+        {{ $ptzSettings->links() }}
+    </div>
 @endsection
