a1e8ad3508e111d26b052a9290819acd1635e44e
ojasvi264 <lalamdorje@gmail.com>
2024-10-24 16:04:07 +0900

    refractor[ptz_info]:adds zoom level value to store in listing page.

--- resources/views/admin/camera-setting/ptz_info.blade.php
+++ resources/views/admin/camera-setting/ptz_info.blade.php
@@ -5,6 +5,7 @@
             <tr>
                 <th>S.N</th>
                 <th>Zoom Level</th>
+                <th>Zoom Value</th>
                 <th>Min PAN</th>
                 <th>Max PAN</th>
                 <th>Min Tilt</th>
@@ -14,6 +15,7 @@
                 <tr>
                     <td>{{ $ptzSettings->firstItem() + $index }}</td>
                     <td>{{$ptzSetting->zoom_level}}</td>
+                    <td>{{$ptzSetting->zoom_level_value}}</td>
                     <td>{{$ptzSetting->pan_limit_min}}</td>
                     <td>{{$ptzSetting->pan_limit_max}}</td>
                     <td>{{$ptzSetting->tilt_limit_min}}</td>
