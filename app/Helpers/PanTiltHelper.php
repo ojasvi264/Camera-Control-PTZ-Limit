e8bb5e79e3692cf84c0ea1d5cf419df75cf987c1
ojasvi264 <lalamdorje@gmail.com>
2024-10-25 18:47:07 +0900

    refractor[PanTiltHelper]:adds pan tilt limit data.

---
+++ app/Helpers/PanTiltHelper.php
@@ -0,0 +1,79 @@
+<?php
+
+function getPanTiltLimits(): array
+{
+    return [
+        'x1' => [
+            'panMin' => 0,
+            'panMax' => 0,
+            'tiltMin' => 0,
+            'tiltMax' => 0,
+        ],
+        'x2' => [
+            'panMin' => -15,
+            'panMax' => 15,
+            'tiltMin' => -9,
+            'tiltMax' => 9,
+        ],
+        'x3' => [
+            'panMin' => -20.22,
+            'panMax' => 20.22,
+            'tiltMin' => -12.5,
+            'tiltMax' => 12.5,
+        ],
+        'x4' => [
+            'panMin' => -22.9,
+            'panMax' => 22.9,
+            'tiltMin' => -14,
+            'tiltMax' => 14,
+        ],
+        'x5' => [
+            'panMin' => -24.5,
+            'panMax' => 24.5,
+            'tiltMin' => -15,
+            'tiltMax' => 15,
+        ],
+        'x6' => [
+            'panMin' => -25.7,
+            'panMax' => 25.7,
+            'tiltMin' => -15.59,
+            'tiltMax' => 15.59,
+        ],
+        'x7' => [
+            'panMin' => -26.5,
+            'panMax' => 26.5,
+            'tiltMin' => -15.59,
+            'tiltMax' => 15.59,
+        ],
+        'x8' => [
+            'panMin' => -27.01,
+            'panMax' => 27.01,
+            'tiltMin' => -16.35,
+            'tiltMax' => 16.35,
+        ],
+        'x9' => [
+            'panMin' => -27.5,
+            'panMax' => 27.5,
+            'tiltMin' => -16.6,
+            'tiltMax' => 16.6,
+        ],
+        'x10' => [
+            'panMin' => -27.91,
+            'panMax' => 27.91,
+            'tiltMin' => -16.8,
+            'tiltMax' => 16.8,
+        ],
+        'x11' => [
+            'panMin' => -28.21,
+            'panMax' => 28.21,
+            'tiltMin' => -16.94,
+            'tiltMax' => 16.94,
+        ],
+        'x12' => [
+            'panMin' => -28.5,
+            'panMax' => 28.5,
+            'tiltMin' => -17.2,
+            'tiltMax' => 17.2,
+        ],
+    ];
+}
