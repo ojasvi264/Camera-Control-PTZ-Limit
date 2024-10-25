1165ba82c1a369ba7edb1991e7de8bd7f65525d6
ojasvi264 <lalamdorje@gmail.com>
2024-10-25 18:49:00 +0900

    refractor[ZoomValue]:adds enum with zoom level values.

---
+++ app/Enum/ZoomValue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enum;
+
+enum ZoomValue: int
+{
+    case x1 = 1;
+    case x2 = 257;
+    case x3 = 513;
+    case x4 = 770;
+    case x5 = 1026;
+    case x6 = 1282;
+    case x7 = 1539;
+    case x8 = 1795;
+    case x9 = 2051;
+    case x10 = 2308;
+    case x11 = 2564;
+    case x12 = 2820;
+}
