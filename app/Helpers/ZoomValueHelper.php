e6bfbf41a924e28bbcadf1bd7de69118fe647eae
ojasvi264 <lalamdorje@gmail.com>
2024-10-29 12:39:07 +0900

    feature[ZoomValueHelper]:adds zoom value helper.

---
+++ app/Helpers/ZoomValueHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+function getZoomValues(): array
+{
+    return [
+        [
+            'zoom' => 2,
+            'value' => 257,
+        ],
+        [
+            'zoom' => 3,
+            'value' => 513,
+
+        ],
+        [
+            'zoom' => 4,
+            'value' => 770,
+        ],
+        [
+            'zoom' => 5,
+            'value' => 1026,
+
+        ],
+        [
+            'zoom' => 6,
+            'value' => 1282,
+        ],
+        [
+            'zoom' => 7,
+            'value' => 1539,
+        ],
+        [
+            'zoom' => 8,
+            'value' => 1795,
+        ],
+        [
+            'zoom' => 9,
+            'value' => 2051,
+        ],
+        [
+            'zoom' => 10,
+            'value' => 2308,
+        ],
+        [
+            'zoom' => 11,
+            'value' => 2564,
+        ],
+        [
+            'zoom' => 12,
+            'value' => 2820,
+        ],
+    ];
+}
