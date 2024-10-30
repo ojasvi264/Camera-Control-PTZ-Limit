89042d5273943342b6a8baf1ef7dafb11ee5952f
ojasvi264 <lalamdorje@gmail.com>
2024-10-30 14:18:35 +0900

    refractor[PtzLimitController]:removes unnecessary code.

--- app/Http/Controllers/PtzLimitController.php
+++ app/Http/Controllers/PtzLimitController.php
@@ -3,76 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Enum\ZoomValue;
+use App\Models\CameraSetting;
 use GuzzleHttp\Client;
 
 
 class PtzLimitController extends Controller
 {
     public function limitPTZ(){
-
-        $minPan1x = -90;
-        $maxPan1x = 180;
-        $minTilt1x = -45;
-        $maxTilt1x = 0;
         $cameraIP = "192.168.128.153";
         $username = "saver";
         $password = "5aver5aver";
 
         $client = new Client();
 
-        // Make the request with Digest Authentication
-//        $response = $client->request('GET', "http://$cameraIP/axis-cgi/com/ptz.cgi?query=position", [
-//            'auth' => [$username, $password, 'digest'] // Use Digest Auth
-//        ]);
-//        $responseBody = (string) $response->getBody();
-//        $ptzInfo = $this->parseResponseToJson($responseBody);
+//         Make the request with Digest Authentication to get the current PTZ info.
+        $response = $client->request('GET', "http://$cameraIP/axis-cgi/com/ptz.cgi?query=position", [
+            'auth' => [$username, $password, 'digest'] // Use Digest Auth
+        ]);
+        $responseBody = (string) $response->getBody();
+        $ptzInfo = $this->parseResponseToJson($responseBody);
 
-        $ptzInfo = [
-                'zoom' => 257,
-        ];
+//        $ptzInfo = [
+//                'zoom' => 257,
+//        ];
+        foreach (getZoomValues() as $zoom){
+            if ($ptzInfo['zoom'] == $zoom['value']);{
+                $ptLimits = CameraSetting::where('zoom_level', $zoom['zoom'])->first();
+                $maxPanLimit = $ptLimits->pan_limit_max;
+                $minPanLimit = $ptLimits->pan_limit_min;
+                $minTiltLimit = $ptLimits->tilt_limit_min;
+                $maxTiltLimit = $ptLimits->tilt_limit_max;
 
-        $zoomLevel = ZoomValue::getZoomLevelFromValue($ptzInfo['zoom'])->name;
-        $limits = getPanTiltLimits();
-//        $matchingLimit = null;
+                $response = $client->request('GET', "http://$cameraIP/axis-cgi/param.cgi?action=update&root.PTZ.Limit.L1.MaxPan=$maxPanLimit&root.PTZ.Limit.L1.MinPan=$minPanLimit&root.PTZ.Limit.L1.MinTilt=$minTiltLimit&root.PTZ.Limit.L1.MaxTilt=$maxTiltLimit", [
+                    'auth' => [$username, $password, 'digest'] // Use Digest Auth
+                ]);
 
-//        foreach (ZoomValue::cases() as $limit) {
-//            if ($ptzInfo['zoom'] == $limit->value) {
-//                $matchingLimit = $limit;
-//                break;
-//            }
-//        }
-//        dd($matchingLimit);
+                $status = $response->getStatusCode();
 
-        if ($zoomLevel){
-            $minPanLimit = $minPan1x + ($limits[$zoomLevel]['panMin']);
-            $maxPanLimit = $maxPan1x + ($limits[$zoomLevel]['panMax']);
+                if ($status === 200){
+                    dd("Pan Limit Changed Successfully. MinPan: $minPanLimit, MaxPan: $maxPanLimit, MinTilt: $minTiltLimit, MaxTilt: $maxTiltLimit");
+                } else {
+                    dd("Failed to change pan limit. Status code: $status");
+                }
 
-            $minTiltLimit = $minTilt1x + ($limits[$zoomLevel]['tiltMin']);
-            $maxTiltLimit = $maxTilt1x + ($limits[$zoomLevel]['tiltMax']);
-
-
-            // Adjust the pan limits to stay within -180 to 180
-            $minPanLimit = round(getAdjustedPanLimit($minPanLimit));
-            $maxPanLimit = round(getAdjustedPanLimit($maxPanLimit));
-
-            $minTiltLimit = round(getAdjustedTiltLimit($minTiltLimit));
-            $maxTiltLimit = round(getAdjustedTiltLimit($maxTiltLimit));
-
-
-            $response = $client->request('GET', "http://$cameraIP/axis-cgi/param.cgi?action=update&root.PTZ.Limit.L1.MaxPan=$maxPanLimit&root.PTZ.Limit.L1.MinPan=$minPanLimit&root.PTZ.Limit.L1.MinTilt=$minTiltLimit&root.PTZ.Limit.L1.MaxTilt=$maxTiltLimit", [
-                'auth' => [$username, $password, 'digest'] // Use Digest Auth
-            ]);
-
-            $status = $response->getStatusCode();
-
-            if ($status === 200){
-                dd("Pan Limit Changed Successfully. MinPan: $minPanLimit, MaxPan: $maxPanLimit, MinTilt: $minTiltLimit, MaxTilt: $maxTiltLimit");
-            } else {
-                dd("Failed to change pan limit. Status code: $status");
             }
-
-        } else{
-            dd("Zoom Level not Matched. Please check again.");
         }
     }
 
