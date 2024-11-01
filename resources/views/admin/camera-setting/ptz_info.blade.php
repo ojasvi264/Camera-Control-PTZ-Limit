71f4422a1e302ba659b0886f88445b70c0d76bad
ojasvi264 <lalamdorje@gmail.com>
2024-11-01 17:59:23 +0900

    adds measurement unit in listing page.

--- resources/views/admin/camera-setting/ptz_info.blade.php
+++ resources/views/admin/camera-setting/ptz_info.blade.php
@@ -16,12 +16,12 @@
                 <tr>
                     <td>{{ $ptzSettings->firstItem() + $index }}</td>
                     <td>{{$ptzSetting->zoom_level}}</td>
-                    <td>{{$ptzSetting->hfov_left_right}}</td>
-                    <td>{{$ptzSetting->vfov_up_down}}</td>
-                    <td>{{$ptzSetting->pan_limit_min}}</td>
-                    <td>{{$ptzSetting->pan_limit_max}}</td>
-                    <td>{{$ptzSetting->tilt_limit_min}}</td>
-                    <td>{{$ptzSetting->tilt_limit_max}}</td>
+                    <td>{{$ptzSetting->hfov_left_right}}(mm)</td>
+                    <td>{{$ptzSetting->vfov_up_down}}(mm)</td>
+                    <td>{{$ptzSetting->pan_limit_min}}<span>&#176;</span></td>
+                    <td>{{$ptzSetting->pan_limit_max}}<span>&#176;</span></td>
+                    <td>{{$ptzSetting->tilt_limit_min}}<span>&#176;</span></td>
+                    <td>{{$ptzSetting->tilt_limit_max}}<span>&#176;</span></td>
                 </tr>
             @endforeach
         </table>
